0bd0f16ab66e3f81e1f52b35882f423317cfa707
gui <guihm.dev@gmail.com>
2024-03-16 13:39:58 -0300

    move to frontend folder

--- frontend/src/settings.js
+++ frontend/src/settings.js
@@ -1,7 +1,7 @@
 export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost.com/api"
 
 export const settings = {
-  siteName: "Fuzzy Train",
+  siteName: "",
   pages: [
     {
       title: "Home",
