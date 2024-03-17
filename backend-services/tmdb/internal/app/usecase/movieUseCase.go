7f07a381504fcda0a3a09d05e7e195fc5cac495a
gui <guihm.dev@gmail.com>
2024-03-17 18:42:01 -0300

    create the movie use case

---
+++ backend-services/tmdb/internal/app/usecase/movieUseCase.go
@@ -0,0 +1,24 @@
+package usecase
+
+import (
+	"tmdb/internal/app/domain"
+	"tmdb/internal/app/ports"
+)
+
+type MovieUseCase struct {
+	movieService ports.MovieService
+}
+
+func NewMovieUseCase(movieService ports.MovieService) *MovieUseCase {
+	return &MovieUseCase{
+		movieService: movieService,
+	}
+}
+
+func (uc *MovieUseCase) GetMovieDetails(id string) (*domain.Movie, error) {
+	return uc.movieService.MovieByID(id)
+}
+
+func (uc *MovieUseCase) FindMovies(query string) ([]*domain.Movie, error) {
+	return uc.movieService.SearchMovies(query)
+}
