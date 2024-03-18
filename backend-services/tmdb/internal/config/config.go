366d567472778eb69ff1f0210149a518d45f5a08
gui <guihm.dev@gmail.com>
2024-03-18 19:15:32 -0300

    update config file

--- backend-services/tmdb/internal/config/config.go
+++ backend-services/tmdb/internal/config/config.go
@@ -1,14 +1,22 @@
 package config
 
-import "os"
+import (
+	"os"
+	"strconv"
+)
 
 type Config struct {
-	APIKey string
+	Port       string
+	APIKey     string
+	APITimeout int
 }
 
 func LoadConfig() *Config {
+
 	return &Config{
-		APIKey: loadEnv("API_KEY", ""),
+		Port:       loadEnv("PORT", "8080"),
+		APIKey:     loadEnv("API_KEY", ""),
+		APITimeout: loadIntEnv("API_TIMEOUT", 5),
 	}
 }
 
@@ -16,6 +24,15 @@ func loadEnv(key, fallback string) string {
 	value, exists := os.LookupEnv(key)
 	if exists {
 		return value
+	}
+	return fallback
+}
+
+func loadIntEnv(key string, fallback int) int {
+	value, exists := os.LookupEnv(key)
+	if exists {
+		i, _ := strconv.Atoi(value)
+		return i
 	}
 	return fallback
 }
