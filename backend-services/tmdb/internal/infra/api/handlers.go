e9f98cc821f551b3d77ac2a6f1a398128b5686b8
gui <guihm.dev@gmail.com>
2024-03-18 19:12:52 -0300

    create api handlers

--- backend-services/tmdb/internal/infra/api/handlers.go
+++ backend-services/tmdb/internal/infra/api/handlers.go
@@ -1,7 +1,13 @@
 package api
 
 import (
+	"encoding/json"
+	"errors"
+	"github.com/go-chi/chi/v5"
+	"net/http"
+	"strconv"
 	"tmdb/internal/app/usecase"
+	"tmdb/pkg/errs"
 )
 
 type TMDBHandler struct {
@@ -14,10 +20,61 @@ func NewTMDBHandler(movieUseCase *usecase.MovieUseCase) *TMDBHandler {
 	}
 }
 
-func (h *TMDBHandler) MovieByID() {
-	// call use case and handle responses
+func (h *TMDBHandler) MovieByID(w http.ResponseWriter, r *http.Request) {
+	movieID := chi.URLParam(r, "id")
+	movieResponse, err := h.movieUseCase.MovieDetails(movieID)
+	if err != nil {
+		handleError(w, err)
+		return
+	}
+	handleResponse(w, movieResponse)
 }
 
-func (h *TMDBHandler) SearchMovies() {
-	// call use case and handle responses
+func (h *TMDBHandler) SearchMovies(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query().Get("query")
+	page, err := strconv.Atoi(r.URL.Query().Get("page"))
+	if err != nil {
+		page = 1
+	}
+	moviesResponse, err := h.movieUseCase.FindMovies(query, page)
+	if err != nil {
+		handleError(w, err)
+		return
+	}
+	handleResponse(w, moviesResponse)
+}
+
+func handleResponse(w http.ResponseWriter, data any) {
+	response, err := json.Marshal(data)
+	if err != nil {
+		handleApiErrors(w, http.StatusInternalServerError, "")
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write(response)
+}
+
+func handleError(w http.ResponseWriter, err error) {
+	if err != nil {
+		var apiError *errs.APIError
+		switch {
+		case errors.As(err, &apiError):
+			handleApiErrors(w, apiError.StatusCode, apiError.Message)
+		default:
+			handleApiErrors(w, http.StatusInternalServerError, "")
+		}
+		return
+	}
+}
+
+func handleApiErrors(w http.ResponseWriter, status int, message string) {
+	if message == "" {
+		message = http.StatusText(status)
+	}
+
+	response, _ := json.Marshal(struct {
+		Error string `json:"error"`
+	}{message})
+	w.WriteHeader(status)
+	_, _ = w.Write(response)
 }
