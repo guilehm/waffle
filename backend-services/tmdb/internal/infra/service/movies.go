2f450489630d09f7148042a536fff82486ede5d0
gui <guihm.dev@gmail.com>
2024-03-18 19:09:47 -0300

    create movie service

---
+++ backend-services/tmdb/internal/infra/service/movies.go
@@ -0,0 +1,55 @@
+package service
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/url"
+	"strconv"
+	"tmdb/internal/app/domain"
+)
+
+func (c *TMDBAPIClient) MovieByID(id string) (*domain.Movie, error) {
+	// make request, decode response to domain.Movie
+	u := c.MakeURL(EndpointMovies.Detail(id), nil)
+	req, err := c.CreateRequest(http.MethodGet, u, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := c.MakeRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	var movie *domain.Movie
+	err = json.Unmarshal(body, &movie)
+	if err != nil {
+		return nil, err
+	}
+	return movie, nil
+}
+
+func (c *TMDBAPIClient) SearchMovies(query string, page int) (*domain.MovieSearchResponse, error) {
+	params := &url.Values{
+		"page":  []string{strconv.Itoa(page)},
+		"query": []string{url.QueryEscape(query)},
+	}
+	u := c.MakeURL(EndpointMovies.Search(), params)
+
+	req, err := c.CreateRequest(http.MethodGet, u, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := c.MakeRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	var movieSearchResponse *domain.MovieSearchResponse
+	err = json.Unmarshal(body, &movieSearchResponse)
+	if err != nil {
+		return nil, err
+	}
+	return movieSearchResponse, nil
+}
