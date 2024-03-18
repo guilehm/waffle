af913c35ebd094ee66da81ce50c991b931e5046f
gui <guihm.dev@gmail.com>
2024-03-18 19:12:21 -0300

    setup router

--- backend-services/tmdb/internal/infra/api/router.go
+++ backend-services/tmdb/internal/infra/api/router.go
@@ -1,7 +1,26 @@
 package api
 
-import "tmdb/internal/app/usecase"
+import (
+	"github.com/go-chi/chi/v5"
+	"net/http"
+	"tmdb/internal/app/usecase"
+)
 
-func SetupRouter(movieUseCase *usecase.MovieUseCase) {
-	return
+func SetupRouter(movieUseCase *usecase.MovieUseCase) *chi.Mux {
+	// create handlers
+	tmdbHandler := NewTMDBHandler(movieUseCase)
+
+	// create router
+	r := chi.NewRouter()
+
+	// set middlewares
+	r.Use(LogRequest)
+
+	// set routes
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte("eat waffles"))
+	})
+	r.Route("/movies", MoviesRouter(tmdbHandler))
+
+	return r
 }
