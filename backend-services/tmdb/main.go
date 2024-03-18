52ea99c87136ca32d91ff0e04029b8224cb46870
gui <guihm.dev@gmail.com>
2024-03-18 19:14:37 -0300

    update main

--- backend-services/tmdb/main.go
+++ backend-services/tmdb/main.go
@@ -2,14 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"tmdb/internal/app/usecase"
+	"tmdb/internal/config"
+	"tmdb/internal/infra/api"
+	"tmdb/internal/infra/server"
+	"tmdb/internal/infra/service"
 )
 
 func main() {
-	//cfg := config.LoadConfig()
-
-	//movieService := service.NewMovieAPIClient("", cfg.APIKey)
-	//movieUseCase := usecase.NewMovieUseCase(movieService)
-	//movieRepo := repo.NewMovieRepo()
-
 	fmt.Println("hello world")
+
+	cfg := config.LoadConfig()
+	movieService := service.NewTMDBAPIClient(cfg.APIKey, cfg.APITimeout)
+	movieUseCase := usecase.NewMovieUseCase(movieService)
+
+	router := api.SetupRouter(movieUseCase)
+	s, listener, err := server.CreateServer(router, cfg.Port)
+	if err != nil {
+		log.Fatalf("could not create server: %v", err)
+	}
+
+	err = s.Serve(*listener)
+	if err != nil {
+		log.Fatalf("could not start server: %v", err)
+	}
 }
