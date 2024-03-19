d026b40f51fbda700edd874719bdeacd864074cf
gui <guihm.dev@gmail.com>
2024-03-18 22:01:04 -0300

    create producer

---
+++ backend-services/tmdb/internal/infra/messaging/kafka/producer.go
@@ -0,0 +1,48 @@
+package kafka
+
+import (
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"log/slog"
+	"tmdb/pkg/logging"
+)
+
+type Producer struct {
+	producer *kafka.Producer
+}
+
+func NewProducer(brokers string) (*Producer, error) {
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": brokers})
+	if err != nil {
+		return nil, err
+	}
+	return &Producer{producer: p}, nil
+}
+
+func (p *Producer) Publish(topic string, message []byte) error {
+	logger := logging.Logger
+	deliveryChan := make(chan kafka.Event, 1)
+
+	err := p.producer.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Value:          message,
+	}, deliveryChan)
+	if err != nil {
+		logger.Error("failed to send message to Kafka", slog.String("error", err.Error()))
+		return err
+	}
+
+	e := <-deliveryChan
+	m := e.(*kafka.Message)
+	if m.TopicPartition.Error != nil {
+		logger.Error("delivery failed", slog.String("error", m.TopicPartition.Error.Error()))
+		return m.TopicPartition.Error
+	}
+
+	logger.Info(
+		"successfully delivered message",
+		slog.String("topic", topic),
+		slog.Int("partition", int(m.TopicPartition.Partition)),
+		slog.String("offset", m.TopicPartition.Offset.String()),
+	)
+	return nil
+}
