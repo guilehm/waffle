24e1f2869d0c4f445b7e68d96abf1fb1d167fbeb
gui <guihm.dev@gmail.com>
2024-03-25 21:45:40 -0300

    handle extra events

--- backend-services/tmdb/internal/infra/messaging/kafka/consumer.go
+++ backend-services/tmdb/internal/infra/messaging/kafka/consumer.go
@@ -8,10 +8,11 @@ import (
 )
 
 type Consumer struct {
-	consumer *kafka.Consumer
+	consumer       *kafka.Consumer
+	minCommitCount int
 }
 
-func NewConsumer(brokers, groupID string) (*Consumer, error) {
+func NewConsumer(brokers, groupID string, minCommitCount int) (*Consumer, error) {
 	logger := logging.Logger
 	c, err := kafka.NewConsumer(&kafka.ConfigMap{
 		"bootstrap.servers":               brokers,
@@ -23,10 +24,10 @@ func NewConsumer(brokers, groupID string) (*Consumer, error) {
 		logger.Error("failed to create consumer", slog.String("error", err.Error()))
 		return nil, err
 	}
-	return &Consumer{consumer: c}, nil
+	return &Consumer{consumer: c, minCommitCount: minCommitCount}, nil
 }
 
-func (c *Consumer) Consume(topics []string, handleMessage func(*kafka.Message)) error {
+func (c *Consumer) Consume(topics []string) error {
 	logger := logging.Logger
 
 	err := c.consumer.SubscribeTopics(topics, nil)
@@ -39,13 +40,35 @@ func (c *Consumer) Consume(topics []string, handleMessage func(*kafka.Message)) 
 	}
 
 	run := true
+	msgCount := 0
 	for run == true {
 		ev := c.consumer.Poll(100)
 		switch e := ev.(type) {
+		case kafka.AssignedPartitions:
+			logger.Info(
+				"rebalance: assigned partitions",
+				slog.String("event", fmt.Sprintf("%v", e)),
+			)
+			_ = c.consumer.Assign(e.Partitions)
+		case kafka.RevokedPartitions:
+			logger.Info(
+				"rebalance: revoked partitions",
+				slog.String("event", fmt.Sprintf("%v", e)),
+			)
+			_ = c.consumer.Unassign()
 		case *kafka.Message:
+			msgCount += 1
+			if msgCount%c.minCommitCount == 0 {
+				_, _ = c.consumer.Commit()
+			}
 			logger.Info(
 				"message consumed",
 				slog.String("topic_partition", fmt.Sprintf("%v", e.TopicPartition)),
+			)
+		case kafka.PartitionEOF:
+			logger.Info(
+				"reached end of partition",
+				slog.String("event", fmt.Sprintf("%v", e)),
 			)
 		case kafka.Error:
 			logger.Error(
